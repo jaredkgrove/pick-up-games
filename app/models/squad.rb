09793258694230ffabc439220bdf56f04cef6f6d
Jared Grove <jaredkgrove@gmail.com>
2019-08-13 17:24:49 -0500

    generated game_players model

--- app/models/squad.rb
+++ app/models/squad.rb
@@ -1,2 +1,4 @@
 class Squad < ApplicationRecord
+    has_many :squad_players
+    has_many :players, through: :squad_players
 end
--- app/models/squad_player.rb
+++ app/models/squad_player.rb
@@ -1,2 +1,4 @@
 class SquadPlayer < ApplicationRecord
+    belongs_to :squad 
+    belongs_to :player
 end
